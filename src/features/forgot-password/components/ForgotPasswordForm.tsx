ed84dbe13bb2f7f2ae99feed8a80b922140ea662
hikwoa-hfw <hibbanhfw@gmail.com>
2025-04-26 23:14:23 +0700

    feat: register, login, forgot-password, reset-password page

---
+++ src/features/forgot-password/components/ForgotPasswordForm.tsx
@@ -0,0 +1,71 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import useForgotPassword from "@/hooks/auth/api/useForgotPassword"; 
+import { cn } from "@/lib/utils";
+import { useFormik } from "formik";
+import { ForgotPasswordSchema } from "../schema";
+
+export function ForgotPasswordForm({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"div">) {
+  const { mutateAsync: ForgotPassword, isPending } = useForgotPassword();
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    validationSchema: ForgotPasswordSchema,
+    onSubmit: async (values) => {
+      await ForgotPassword(values);
+    },
+  });
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl">Forgot Password</CardTitle>
+          <CardDescription>
+            Enter your data below to reset your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={formik.handleSubmit}>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                {!!formik.touched.email && !!formik.errors.email && (
+                  <p className="text-xs text-red-500">{formik.errors.email}</p>
+                )}
+              </div>
+
+              <Button type="submit" className="w-full" disabled={isPending}>
+                {isPending ? "Loading..." : "Send"}
+              </Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
---
+++ src/features/register/components/RegisterForm.tsx
@@ -0,0 +1,113 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import useRegisterUser from "@/hooks/auth/api/useRegisterUser";
+import { cn } from "@/lib/utils";
+import { useFormik } from "formik";
+import Link from "next/link";
+import { RegisterSchema } from "../schemas";
+
+export function RegisterForm({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"div">) {
+  const {mutateAsync: register, isPending} = useRegisterUser();
+
+  const formik = useFormik({
+    initialValues: {
+      fullName: "",
+      password: "",
+      email: "",
+      role:"USER"
+    },
+    validationSchema: RegisterSchema,
+    onSubmit: async (values) => {
+     await register(values)
+    },
+  });
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl">Register</CardTitle>
+          <CardDescription>
+            Enter your email below to register to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={formik.handleSubmit}>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="fullName">Full Name</Label>
+                <Input
+                  id="fullName"
+                  type="fullName"
+                  name="fullName"
+                  placeholder="your name"
+                  required
+                  value={formik.values.fullName}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                {!!formik.touched.fullName && !!formik.errors.fullName && (
+                  <p className="text-xs text-red-500">{formik.errors.fullName}</p>
+                )}
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                {!!formik.touched.email && !!formik.errors.email && (
+                  <p className="text-xs text-red-500">{formik.errors.email}</p>
+                )}
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                {!!formik.touched.password && !!formik.errors.password && (
+                  <p className="text-xs text-red-500">{formik.errors.password}</p>
+                )}
+              </div>
+              <Button type="submit" className="w-full" disabled={isPending}>
+                {isPending? "Loading..." : "Register"}
+              </Button>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Have an account?{" "}
+              <Link href="/login" className="underline underline-offset-4">
+                Sign in
+              </Link>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
